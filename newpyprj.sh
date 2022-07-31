cb260635a512064c6571598a97ef3675beae4699
Martin Kuchynka <kuchynkm@gmail.com>
2022-07-31 09:51:44 +0200

    curl mypy.ini and .flake8

--- newpyprj.sh
+++ newpyprj.sh
@@ -54,9 +54,14 @@ echo "$GITIGNORE" > .gitignore
 poetry add pre-commit --dev
 
 
-# fetch pre-commit config template from my repository
+# fetch yaml template from my pre-commit config repo
 echo $'\nGetting pre-commit config template from https://github.com/kuchynkm/pre-commit-hooks-template.git ...'
 curl --silent --show-error https://raw.githubusercontent.com/kuchynkm/pre-commit-hooks-template/master/.pre-commit-config.yaml  -O
+
+# fetch .flake8 and mypy.ini from my linter config repo
+echo $'\nGetting pre-commit config template from https://github.com/kuchynkm/linter-configs.git ...'
+curl --silent --show-error https://github.com/kuchynkm/linter-configs/raw/master/.flake8  -O
+curl --silent --show-error https://github.com/kuchynkm/linter-configs/raw/master/mypy.ini  -O
 
 
 # setup pre-commit hooks
